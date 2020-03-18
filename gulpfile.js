0d90821bcd94d1055537db6aa28b85a802843ec4
Nicole Kemp <nicole@byteorbit.com>
2020-03-18 20:58:45 +0200

    upgraded to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,27 @@ var reload      = browserSync.reload;
 
 
 /* Task to compile less */
-gulp.task('compile-less', function() {
+gulp.task('compile-less', function(done) {
   gulp.src('./src/init.less')
     .pipe(less())
     .pipe(gulp.dest('./distro/css/'))
     .pipe(browserSync.reload({
       stream: true
     }))
+    done();
 });
 /* Task to watch less changes */
-gulp.task('watch-less', function() {
-  gulp.watch('./src/**/*.less' , ['compile-less']);
+gulp.task('watch-less', function(done) {
+    gulp.watch('./src/**/*.less')
+        .on('change', function(path, stats) {
+            console.log('File ' + path + ' was changed');
+        }).on('unlink', function(path) {
+        console.log('File ' + path + ' was removed');
+    });
+    done();
 });
 
-gulp.task('serve', function () {
-
+gulp.task('serve', function (done) {
     // Serve files from the root of this project
     browserSync.init({
         server: {
@@ -28,7 +34,9 @@ gulp.task('serve', function () {
     });
     gulp.watch("./src/**/*.less").on("change", reload);
     gulp.watch("./distro/*.html").on("change", reload);
+    done();
 });
 
 /* Task when running `gulp` from terminal */
-gulp.task('default', ['watch-less', 'serve']);
+// gulp.task('default', ['watch-less', 'serve']);
+gulp.task('default', gulp.series('watch-less', 'serve'));
