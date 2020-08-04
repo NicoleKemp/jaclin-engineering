5251ce11960f737247a9812326c30f8e985c141c
Nicole Kemp <nicole@byteorbit.com>
2020-08-04 15:09:09 +0200

    navbar updates and home bg update

--- distro/js/main.js
+++ distro/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
     AOS.init();
 
+    $('body').scrollspy({ target: '#bs-example-navbar-collapse-1' });
+
     $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         if( target.length ) {
